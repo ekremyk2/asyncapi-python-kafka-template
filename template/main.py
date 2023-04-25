b0ed5392740f87833a74dedbf6f68f12e044a11b
ekremyk2 <ekremyk2@gmail.com>
2023-04-25 19:48:36 +0300

    endif fix

--- template/main.py
+++ template/main.py
@@ -1,8 +1,8 @@
+from topics.{{channel_name}} import {{channel_name}}Consumer, {{channel_name}}Producer
 from confluent_kafka import Producer
 from confluent_kafka import Consumer
-{% for channel_name, channel_info in asyncapi.channels() %}
-from topics.{{ channel_name }} import {{ channel_name }}Consumer, {{ channel_name }}Producer
-{% endfor %}
+{%- for channel_name, channel_info in asyncapi.channels() %}
+{%- endfor %}
 
 # Set up Kafka configuration
 KAFKA_BROKERS = '{{ asyncapi.servers[0].url }}'
@@ -14,16 +14,16 @@ consumer = Consumer({
     'auto.offset.reset': 'earliest'
 })
 
-{% for channel_name, channel_info in asyncapi.channels() %}
+{%- for channel_name, channel_info in asyncapi.channels() %}
 # Subscribe to the Kafka topic for {{ channel_name }} channel
-{{ channel_name|lower }}_topic = '{{ channel_info.subscribe().topic }}'
-consumer.subscribe([{{ channel_name|lower }}_topic])
+{{channel_name | lower}}_topic = '{{ channel_info.subscribe().topic }}'
+consumer.subscribe([{{channel_name | lower}}_topic])
 
 # Create a {{ channel_name }} consumer and start consuming messages
-{{ channel_name|lower }}_consumer = {{ channel_name }}Consumer(KAFKA_BROKERS, {{ channel_name|lower }}_topic, consumer)
-{{ channel_name|lower }}_consumer.consume_messages()
+{{channel_name | lower}}_consumer = {{channel_name}}Consumer(KAFKA_BROKERS, {{channel_name | lower}}_topic, consumer)
+{{channel_name | lower}}_consumer.consume_messages()
 
 # Create a {{ channel_name }} producer and send a message
-{{ channel_name|lower }}_producer = {{ channel_name }}Producer(KAFKA_BROKERS, {{ channel_name|lower }}_topic, producer)
-{{ channel_name|lower }}_producer.produce_message('Hello, World!')
-{% endfor %}
+{{channel_name | lower}}_producer = {{channel_name}}Producer(KAFKA_BROKERS, {{channel_name | lower}}_topic, producer)
+{{channel_name | lower}}_producer.produce_message('Hello, World!')
+{%- endfor % }
