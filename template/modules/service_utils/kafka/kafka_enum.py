453e44ad6d30bf83cfe5f4d556a1a46d815c5bf7
ekremyk2 <ekremyk2@gmail.com>
2023-04-28 19:05:41 +0300

    update

---
+++ template/modules/service_utils/kafka/kafka_enum.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+{% for schema_name, schema in asyncapi.components().schemas() -%}
+{% for name, prop in schema.properties() -%}
+{% set tpyeInfo = [name, prop] | getTypeInfo -%}
+{% if typeInfo.generalType === 'enum' -%}
+class {{schema_name}}(str, Enum):
+{% for v in typeInfo.enum -%}
+    {{ v }} = '{{ v }}'
+{% endfor -%}
+{%- endif %}
+{%- endfor %}        
+{%- endfor %}
---
+++ template/modules/service_utils/kafka/kafka_result.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+from kafka.kafka_result import *
+
+{% for schema_name, schema in asyncapi.components().schemas() -%}
+@dataclass
+class {{schema_name}}:
+{% for name, prop in schema.properties() %}
+{% set typeInfo = [name, prop] | getTypeInfo -%}
+    {{ typeInfo.pythonName }}{{ ": " + typeInfo.pythonName if typeInfo.pythonName else ""}}
+{%- else %}
+    pass
+{% endfor %}
+{% endfor %}
