a5747040a89da905d71c7d5fa8e589042726dd3d
ekremyk2 <ekremyk2@gmail.com>
2023-04-28 22:21:46 +0300

    Enum

--- template/modules/service_utils/kafka/kafka_enum.py
+++ template/modules/service_utils/kafka/kafka_enum.py
@@ -1,5 +1,11 @@
 from enum import Enum
 
+
+class KafkaAction(str, Enum):
+{% for channel_name, channel in asyncapi.channels() -%}
+{{"    "}}{{ channel_name | snakeCase }} = '{{ channel_name | snakeCase }}'
+{% endfor %}
+
 {% for schema_name, schema in asyncapi.components().schemas() %}
 {% for name, prop in schema.properties() %}
 {% set typeInfo = [name, prop] | getTypeInfo %}
