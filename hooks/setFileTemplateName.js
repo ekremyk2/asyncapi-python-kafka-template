2ef919ed33d616de08c5b8f1ad154a7e33d10a03
ekremyk2 <ekremyk2@gmail.com>
2023-04-28 11:05:32 +0300

    tet

--- hooks/setFileTemplateName.js
+++ hooks/setFileTemplateName.js
@@ -1,10 +1,15 @@
 module.exports = {
   'setFileTemplateName': (generator, hookArguments) => {
-    console.log(generator, hookArguments);
     let currentFilename = hookArguments.originalFilename;
     while (currentFilename.includes('-') || currentFilename.includes('.') || currentFilename.includes('/')){
       currentFilename = currentFilename.replace("-", "_").replace(".", "_").replace("/", "_");
     }
     return currentFilename;
   },
+  'generate:before': (a, b, c) => {
+    console.log("generate:before", a, b, c)
+  },
+  'generate:after': (a, b, c) => {
+    console.log("generate:after", a, b, c)
+  }
 };
