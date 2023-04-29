8afd07ed3acbd1e6bb1f0adebc451ebbc40c4287
ekremyk2 <ekremyk2@gmail.com>
2023-04-29 14:39:11 +0300

    cap fix

--- template/modules/service_utils/kafka.py
+++ template/modules/service_utils/kafka.py
@@ -10,7 +10,7 @@ class KafkaConsumer():
 {% if channel.hasPublish() %}
 {% for message_name, message in channel.messages() %}
 {{"    "}}@on(KafkaAction.{{ message_name | camelCase }})
-{{"    "}}async def on{{ message_name | camelCase, firstUpper }}(self,{% for name, prop in message.payload().properties() %}{{ name }},{% endfor %},**kwargs):
+{{"    "}}async def on_{{ message_name | camelCase, capitalize }}(self,{% for name, prop in message.payload().properties() %}{{ name }},{% endfor %}, **kwargs ):
 {{"    "}}{{"    "}}logging.warning("{% for name, prop in message.payload().properties() %}{{ name }}: %s, {% endfor %}", {% for name, prop in message.payload().properties() %}{{ name }}{% endfor %})
 {{"    "}}{{"    "}}return kafka_result.{{ message.payload()["schema_name"] }}(
 {% for name, prop in message.payload().properties() %}
