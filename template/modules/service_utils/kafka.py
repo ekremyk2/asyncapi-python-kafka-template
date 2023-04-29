c81ce52df7df3c56a897881aed1ef2e5a10378e5
ekremyk2 <ekremyk2@gmail.com>
2023-04-29 14:58:24 +0300

    fix?

--- template/modules/service_utils/kafka.py
+++ template/modules/service_utils/kafka.py
@@ -8,12 +8,12 @@ class KafkaConsumer():
         
 {% for channel_name, channel in asyncapi.channels() %}
 {% if channel.hasPublish() %}
-{% for message_name, message in channel.messages() %}
-{{"    "}}@on(KafkaAction.{{ message_name | camelCase }})
-{{"    "}}async def on_{{ message_name | capitalize }}(self,{% for name, prop in message.payload().properties() %}{{ name }},{% endfor %}, **kwargs ):
-{{"    "}}{{"    "}}logging.warning("{% for name, prop in message.payload().properties() %}{{ name }}: %s, {% endfor %}", {% for name, prop in message.payload().properties() %}{{ name }}{% endfor %})
-{{"    "}}{{"    "}}return kafka_result.{{ message.payload()["schema_name"] }}(
-{% for name, prop in message.payload().properties() %}
+{% set messageInfo = [message_name, message] | channel.publish().message() %}
+{{"    "}}@on(KafkaAction.{{ messageInfo.message_name | camelCase }})
+{{"    "}}async def on_{{ messageInfo.message_name | capitalize }}(self,{% for name, prop in messageInfo.message.payload().properties() %}{{ name }},{% endfor %}, **kwargs ):
+{{"    "}}{{"    "}}logging.warning("{% for name, prop in message.payload().properties() %}{{ name }}: %s, {% endfor %}", {% for name, prop in messageInfo.message.payload().properties() %}{{ name }}{% endfor %})
+{{"    "}}{{"    "}}return kafka_result.{{ messageInfo.message.payload()["schema_name"] }}(
+{% for name, prop in messageInfo.message.payload().properties() %}
 {{"    "}}{{"    "}}{{"    "}}{{ name }} = {{ name }},
 {% endfor %}
 {{"    "}}{{"    "}})
